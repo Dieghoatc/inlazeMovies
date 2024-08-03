90ba320afc371dcfec0eb04bd651f2ea1d6035e8
Dieghoatc <dieghoatc@gmail.com>
2024-08-02 21:34:15 -0500

    fix: View mobile desing

--- frontend/src/app/components/Hero/Hero.tsx
+++ frontend/src/app/components/Hero/Hero.tsx
@@ -1,46 +1,24 @@
 import styles from "./Hero.module.css";
 import Po from "../../assets/images/Po.jpg";
-import { FavoriteLogo } from "./FavoriteLogo";
-import { SavedLogo } from "./SavedLogo";
-import { ShareLogo } from "./ShareLogo";
-import { DotsProgressBar } from "./components/DotsProgressBar";
+
 import { CircularProgressBar } from "./components/CircularProgressBar";
+import { HeroDetails } from "./components/HeroDetails";
 
 export function Hero() {
   return (
     <div className={styles.hero_container}>
-      <div className={styles.hero_information}>
-        <h2>Kung Fu Panda 4</h2>
-        <h3>
-          Join Po and the Furious Five on a new epic adventure! Discover the power of friendship and the strength
-          within! Get ready to unleash your inner warrior! 🥋✨
-        </h3>
+      <div className={styles.hero_container}>
+        <img className={styles.hero_image} src={Po.src} alt="Po" />
+        <div className={styles.hero_information}>
+          <h2>Kung Fu Panda 4</h2>
+          <h3>
+            Join Po and the Furious Five on a new epic adventure! Discover the power of friendship and the strength
+            within! Get ready to unleash your inner warrior! 🥋✨
+          </h3>
+        </div>
       </div>
-
-      <img className={styles.hero_image} src={Po.src} alt="Po" />
       <div className={styles.hero_details}>
-        <div className={styles.hero_menu}>
-          <ul>
-            <li>Trama</li>
-            <li>Cast</li>
-            <li>Gallery</li>
-            <li>Info</li>
-          </ul>
-        </div>
-        <div className={styles.dots_progress_bar}>
-          <DotsProgressBar />
-        </div>
-        <div className={styles.user_selectors}>
-          <div className={styles.favorite_logo}>
-            <FavoriteLogo />
-          </div>
-          <div className={styles.saved_logo}>
-            <SavedLogo />
-          </div>
-          <div className={styles.share_logo}>
-            <ShareLogo />
-          </div>
-        </div>
+        <HeroDetails />
       </div>
       <div className={styles.circle_progress_bar}>
         <CircularProgressBar />
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,24 @@
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
+    <div className={styles.home_wrapper}>
+      <div className={styles.navbar_wrapper}>
+        <Navbar />
+      </div>
+      <div className={styles.hero_wrapper}>
+        <Hero />
+      </div>
+      <div className={styles.content_wrapper}>
+        <div className={styles.aside_wrapper}>
+          <p>Search</p>
+        </div>
+        <div className={styles.sections_wrapper}>
+          <p>Popular</p>
+        </div>
+      </div>
     </div>
   );
 }
